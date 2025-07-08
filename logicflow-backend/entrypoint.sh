119857511633a648aba3a174303b06a60bda3fbb
shivang2000 <shivangchheda2000@gmail.com>
2025-07-08 15:49:39 +0530

    done

--- logicflow-backend/entrypoint.sh
+++ logicflow-backend/entrypoint.sh
@@ -11,4 +11,5 @@ python manage.py collectstatic --no-input
 ls
 
 
-gunicorn mysite.wsgi --bind 0.0.0.0:8000
+# gunicorn mysite.wsgi --bind 0.0.0.0:8000
+gunicorn --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 mysite.asgi:application
--- logicflow-backend/mysite/wsgi.py
+++ logicflow-backend/mysite/wsgi.py
@@ -10,7 +10,26 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+from django.urls import path
+from doc.consumers import DocConsumer
+
+websocket_urlpatterns = [
+    path('ws/doc/shivang/', DocConsumer.as_asgi()),
+]
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_wsgi_application()
+django_wsgi_app = get_wsgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_wsgi_app,
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": 
+            URLRouter(websocket_urlpatterns)
+        ,
+})
