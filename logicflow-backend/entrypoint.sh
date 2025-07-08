8d52ece32974c5c40c7cf0f6971cad72e5095d0e
shivang2000 <shivangchheda2000@gmail.com>
2025-07-08 14:46:29 +0530

    added docker for easy running

---
+++ logicflow-backend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+python manage.py makemigrations doc --no-input
+python manage.py migrate doc --no-input
+python manage.py migrate  --no-input
+python manage.py collectstatic --no-input 
+# python manage.py createsuperuser --username $DJANGO_SUPERUSER_USERNAME  --no-input
+
+ls
+
+
+gunicorn mysite.wsgi --bind 0.0.0.0:8000
